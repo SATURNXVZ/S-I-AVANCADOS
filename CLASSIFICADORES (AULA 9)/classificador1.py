111288c7cf0856b7e0623efb629484ca7d2f91ed
SATURNXVZ <vvitorhenriique@gmail.com>
2026-05-11 21:39:59 -0300

    aula 9 terminada

--- CLASSIFICADORES (AULA 9)/classificador1.py
+++ CLASSIFICADORES (AULA 9)/classificador1.py
@@ -4,17 +4,26 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 #precisamos de um metaestimador
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
 
 
 
 dados =pd.read_csv("fertility_Diagnosis.bolnho")
 
+#separar atributos e classes
 dadosAtributos = dados.drop(columns=['Diagnostico'])
-
 dadosClasses = dados['Diagnostico']
 
+#balancear as classes
+balancer = SMOTE()
+dadosAtributos, dadosClasses = balancer.fit_resample(dadosAtributos, dadosClasses)
+
+#frequencia de classes originais
+print('Frequencia das classes balanceadas: ', dadosClasses.value_counts)
+
+#Segmentar ps dadps em dados para treinamento
 atributoTrain, atributoTest, classeTrain, classeTest = train_test_split(dadosAtributos, dadosClasses, test_size=0.3)
 
 #treinar modelo
@@ -32,3 +41,17 @@ plt.show()
 #acuracia geral 
 acuracia = accuracy_score(classeTest, predicts)
 print('acuracia: ', acuracia)
+
+vn, fn, vp, fp = confusion_matrix(classeTest, predicts).ravel()
+
+#especificidade = vn/(vn+fp)
+especify = vn/(vn+vp)
+
+#sensibilidade = vp/(vp+fn)
+sensibility = vp/(vp+fn)
+
+print('Especifidade: ', especify)
+print('Sensibility: ', sensibility)
+
+
+
